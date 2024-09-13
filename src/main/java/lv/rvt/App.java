11d0c1c55e9d3c56da7659e3568eef07fde3c983
23DP4KLako <166509373+23DP3KLako@users.noreply.github.com>
2024-09-13 11:28:29 +0000

    uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,9 +1,21 @@
 package lv.rvt;
 
+import java.util.*;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        // Make your Hello World programm!
+      
+       Scanner scanner = new Scanner(System.in);
+
+       String name = scanner.nextLine();
+       String surname = scanner.nextLine();
+       String group = scanner.nextLine();
+
+       System.out.println(name);
+       System.out.println(surname);
+       System.out.println(group);
+
     }
 }
