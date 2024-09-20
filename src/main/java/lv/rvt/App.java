7cd9df2fc5d14702db40963f2a42df669b4e7050
23DP4KLako <166509373+23DP3KLako@users.noreply.github.com>
2024-09-20 10:39:04 +0000

    simple calculator

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,16 +6,21 @@ public class App
 {
     public static void main( String[] args )
     {
-      
-       Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Give the first number:");
+        int first = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int second = Integer.valueOf(scanner.nextLine());
 
-       String name = scanner.nextLine();
-       String surname = scanner.nextLine();
-       String group = scanner.nextLine();
-
-       System.out.println(name);
-       System.out.println(surname);
-       System.out.println(group);
-
+        int result1 = first + second;
+        System.out.println("8" + " " + "+" + "2" + " " + "=" + " " + result1);
+        int result2 = first - second;
+        System.out.println("8" + " " + "-" + "2" + " " + "=" + " " + result2);
+        int result3 = first * second;
+        System.out.println("8" + " " + "*" + "2" + " " + "=" + " " + result3);
+        double result4 = first / second;
+        System.out.println("8" + " " + "/" + "2" + " " + "=" + " " + result4);
     }
-}
+
+}   
