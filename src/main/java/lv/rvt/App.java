bb534cc7c905e98ed333e50d6a2ca49a497d5cb7
23DP4KLako <166509373+23DP3KLako@users.noreply.github.com>
2024-10-10 11:24:52 +0000

    Number and sum of numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,30 +6,33 @@ public class App
 {
     public static void main( String[] args ){
         Scanner scanner = new Scanner(System.in);
-       
-        System.out.println("Give points [0 - 100]:");
-        int points = Integer.valueOf(scanner.nextLine());
+        int sk = 0;
+        int sum = 0;
 
-        if (points < 0) {
-            System.out.println("Grade:" + " " + "inpossible!");
-        } else if (points < 49){
-            System.out.println("Grade:" + " " + "failed");
-        } else if (points <= 50 && points <= 59){
-            System.out.println("Grade: 1");
-        } else if (points <= 60 && points <= 69){
-            System.out.println("Grade: 2");
-        } else if (points <= 70 && points <= 79){
-            System.out.println("Grade: 3");
-        } else if (points <= 80 && points <= 89){
-            System.out.println("Grade: 4");
-        } else if (points <= 90 && points <= 100){
-            System.out.println("Grade: 5");
-        } else {
-            System.out.println("Grade: incredible!");
+        while (true) {
+            System.out.println("Give a number");
+            int num = Integer.valueOf(scanner.nextLine());
+
+            if (num == 0) {
+                break;
+            }
+
+            if (num > 0 || num < 0) {
+                sk = sk + 1;
+                sum = sum + num;
+            }
+
+
         }
        
-       }
- 
+        if (sk != 0) {
+
+            System.out.println("Number of numbers: " + sk);
+            System.out.println("Sum of number: " + sum);
+        }
+       
     }
+ 
+}
 
   
