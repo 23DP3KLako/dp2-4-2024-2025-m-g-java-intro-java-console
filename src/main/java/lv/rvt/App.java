e61db93e23ac2dfc7d29f46d8e064cd98e2ed76c
23DP4KLako <166509373+23DP3KLako@users.noreply.github.com>
2024-10-11 11:11:44 +0000

    Reprint

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,44 +7,17 @@ public class App
     public static void main( String[] args ) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Give numbers: ");
-
-        int sum = 0;
-        int numbers = 0;
-        int even = 0;
-        int odd = 0;
-
-
-    while (true) {
-
-
-        int UserInput = Integer.valueOf(scanner.nextLine());
-
-
-        if (UserInput == -1){
-            float avg = sum / numbers;
-            System.out.println("Thx! Bye!");
-            System.out.println("Sum" + " " + sum);
-            System.out.println("Numbers:" + numbers);
-            System.out.println("Average:" + avg);
-            System.out.println("Even" + " " + even);
-            System.out.println("Odd" + " " + odd);
-            break;
-        
-        }
-        else {
-            sum += UserInput;
-            numbers++;
+        System.out.println("How many times?");
+        int numb = Integer.valueOf(scanner.nextLine());
+        int i = 0;
+        for(i = 0; i < numb; i++) {
+            printText();
         }
         
-        if (UserInput % 2 == 0){
-            even++;
-        }
-        else {
-            odd++;
-        }
-        
+
     }
+    public static void printText() {
+        System.out.println("In a hole in the ground there lived a method");
     }
  
 }
