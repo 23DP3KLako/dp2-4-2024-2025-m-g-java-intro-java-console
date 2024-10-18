4cf3ae0c0c2c98be0536ba7baaa0ae2298ac3c65
23DP4KLako <166509373+23DP3KLako@users.noreply.github.com>
2024-10-18 10:29:42 +0000

    Star sign (2 part)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,9 +6,10 @@ public class App
 {
     public static void main( String[] args ) {
         Scanner scanner = new Scanner(System.in);
-        printStars(5);
-        printStars(3);
-        printStars(9);
+        printStars(4);
+        printStars(4);
+        printStars(4);
+        printStars(4);
 
     }
     
