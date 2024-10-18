f3267039c9d89c2ba907822b6e6183c160483d28
23DP4KLako <166509373+23DP3KLako@users.noreply.github.com>
2024-10-18 10:40:45 +0000

    Star sign (3 part)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,18 +6,18 @@ public class App
 {
     public static void main( String[] args ) {
         Scanner scanner = new Scanner(System.in);
-        printStars(4);
-        printStars(4);
-        printStars(4);
-        printStars(4);
+        printRectangle(17, 3);
 
     }
     
-    public static void printStars(int number) {
-        for(int i = 0; i < number; i++){
-            System.out.print("*");
+    public static void printRectangle(int width, int height) {
+        for(int i = 0; i < height; i++){
+            for(int k = 0; k < width; k++){
+                System.out.print("*");
+            }
+            System.out.println("");
         }
-        System.out.println("");
+        
     }
 }
 
